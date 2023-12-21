1ba3efd4647d5d4edf5476f5938f9223c51e227a
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-12-21 14:29:26 +0100

    Add missing exceptions file.
    Two custom exceptions are included:
    * UnknownTerm : some term is unknown within the VSM
    * UnknownDocument : some document (identified by a unique ID)
                        is unknown within the document store

---
+++ exceptions.py
@@ -0,0 +1,15 @@
+class UnknownTerm(Exception):
+    def __init__(self, term: str, *args: object) -> None:
+        super().__init__(*args)
+        self.term = term
+
+    def __str__(self) -> str:
+        return f"'{self.term}'"
+
+class UnknownDocument(Exception):
+    def __init__(self, document_ID: int, *args: object) -> None:
+        super().__init__(*args)
+        self.document_ID = document_ID
+
+    def __str__(self) -> str:
+        return f"document ID = {self.document_ID}"
