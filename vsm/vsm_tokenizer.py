0b2338419fd38a46f40fda60d34376083a584409
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-12-24 14:24:21 +0100

    Rework VSMTokenizer to be marginally more performant.
    VSMTokenizer edits:
    * Add basic docstring to class
    * Reorganize __init__ into: text processors: tokenizer, stemmer, inflect_engine
    * Remove unneeded compiled regex patterns from __init__
    * Use token-based instead of regex based contraction expansion -> move to token
      based processing section of tokenize
    * Use regex-based instead of token based numbers_to_words converstion -> move
      to string based tokenize section
    * Special character remove MUST happen after contraction expansion, so alter
      it to make use of a simplistic tokens->string->tokens conversion process
      -> move special character remove to token based section of tokenize
    * Make stemming optional --> STEMMING IS MAJOR PART OF PROCESSING TIME
    * Rework all pre-processing helper methods to support the changes mentioned
      above
    * Remove unused VSMTokenizer._is_number(str)

--- vsm/vsm_tokenizer.py
+++ vsm/vsm_tokenizer.py
@@ -2,37 +2,50 @@ import re
 import nltk
 import inflect
 
-from typing import Set, List, Dict, Union
+from typing import Set, List, Dict
 from unidecode import unidecode
 from contractions import CONTRACTION_MAP
 
 
 
 class VSMTokenizer:
+    """A pre-processor class to normalize text for use in a VSM.
+    This pre-processor performs the following steps, not necessarily in this order:
+    - replace accented characters   (êË => eE)
+    - convert text to lower case    (eE => ee)
+    - convert numbers to their word form (12 => twelve)
+    - expand contractions   (i'd => i would)
+    - remove special characters (any char not in [a-zA-Z0-9\s] where [0-9] is optional)
+    - stemming              (optional, eat => eat, eating => eat, eaten => eat)
+    - remove stopwords      (remove {i, you, and, the, ...})
+    """
     def __init__(self) -> None:
-        self._stemmer = nltk.porter.PorterStemmer()
-        self._stopword_list: Set[str] = set()
-        self._load_stopwords()
-        self._tokenizer = nltk.tokenize.toktok.ToktokTokenizer()
+        # Text processors
+        self._tokenizer: nltk.ToktokTokenizer = nltk.ToktokTokenizer()      # ToktokTokenizer is slower than str.split(), but better results?
+        self._stemmer: nltk.LancasterStemmer = nltk.LancasterStemmer()      # PorterStemmer is faster than the LancasterStemmer, but better results?
         self._inflect_engine: inflect.engine = inflect.engine()
+
+        # Reference data
+        self._stopword_list: Set[str] = self._load_stopwords()
         self._contraction_mapping: Dict[str, str] = CONTRACTION_MAP
 
         # Regex patterns
-        self._CONTRACTION_PATTERN = re.compile('({})'.format('|'.join(self._contraction_mapping.keys())),
-                                               flags=re.IGNORECASE|re.DOTALL)
+        self._NUMBER_PATTERN: re.Pattern       = re.compile(r'[+-]?(\d+([.]\d*)?|[.]\d+)')    # Matches whole numbers and floats of the form 'x.', '.y', 'x.y'
+        self._NORMAL_ALNUM_PATTERN: re.Pattern = re.compile(r'[^a-zA-z0-9\s]')
+        self._NORMAL_ALPHA_PATTERN: re.Pattern = re.compile(r'[^a-zA-z\s]')
 
-    def tokenize(self, text: str) -> List[str]:
+    def tokenize(self, text: str, do_stemming: bool = True) -> List[str]:
         # String-based pre-processing
         text = self._remove_accents(text, strict=True)
-        text = self._expand_contractions(text)
         text = text.lower()     # Change case should only be done if the following steps will not add wrong-case characters
+        text = self._numbers_to_words(text)
 
         # Token-based pre-processing
-        tokens: List[str] = self._tokenizer.tokenize(text)
-        tokens = self._numbers_to_words(tokens)
-        tokens = self._remove_special_characters(tokens, replace_char=' ', remove_digits=False)  # replacing by ' ' instead of '' is important for hyphenated compound words, e.g. "twenty-three", "know-how", ...
-        tokens = self._stem(tokens)
-        tokens = self._remove_stopwords(tokens)
+        tokens: List[str] = text.split()
+        tokens = self._expand_contractions(tokens)      # regex-based contraction expansion is slow?
+        tokens = self._remove_special_characters(tokens, replace_char=' ', remove_digits=False)
+        if do_stemming: tokens = self._stem(tokens)
+        tokens = self._remove_stopwords(tokens, is_lower_case=True)
 
         return tokens
 
@@ -49,29 +62,21 @@ class VSMTokenizer:
         """
         return unidecode(text, errors="strict" if strict else "ignore")
 
-    def _expand_contractions(self, text: str):
-        """Expand all contractions in the *text* using the VSMTokenizer's internal contraction map.
+    def _expand_contractions(self, tokens: List[str]) -> List[str]:
+        """Expand all contractions in the *tokens* using the VSMTokenizer's internal contraction map.
         
-        This entire method was obtained from the following article:
+        This method was obtained from the following article and subsequently adapted:
             https://www.kdnuggets.com/2018/08/practitioners-guide-processing-understanding-text-2.html
 
-        :param text: The text containing contractions, to expand
-        :return: The *text* where all contractions have been expanded
+        :param tokens: The tokens list containing contractions, to expand
+        :return: The *tokens* where all contractions have been expanded
         """
-        def expand_match(contraction):
-            match = contraction.group(0)
-            first_char = match[0]
-            expanded_contraction = self._contraction_mapping.get(match)\
-                                    if self._contraction_mapping.get(match)\
-                                    else self._contraction_mapping.get(match.lower())                       
-            expanded_contraction = first_char+expanded_contraction[1:]
-            return expanded_contraction
-            
-        expanded_text: str = self._CONTRACTION_PATTERN.sub(expand_match, text)
-        expanded_text = expanded_text.replace("'", "")
-        return expanded_text
+        return [
+            self._contraction_mapping.get(token, token)
+            for token in tokens
+        ]
 
-    def _remove_special_characters(self, tokens: List[str], replace_char: str = '', remove_digits: bool = False):
+    def _remove_special_characters(self, tokens: List[str], replace_char: str = '', remove_digits: bool = False) -> List[str]:
         """Remove all non-alpha-numeric characters from the *tokens*.
         
         This method was obtained from the following article and subsequently adapted:
@@ -83,42 +88,47 @@ class VSMTokenizer:
         :param remove_digits: Whether to also remove digits
         :return: The *tokens* where all special characters have been removed
         """
-        pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
-        return [
-            new_token
-            for token in tokens
-                for new_token in re.sub(pattern, replace_char, token).strip().split(' ')
-                if new_token != ''
-        ]
+        pattern = self._NORMAL_ALNUM_PATTERN if not remove_digits else self._NORMAL_ALPHA_PATTERN
+        return pattern.sub(replace_char, ' '.join(tokens)).split()
 
-    def _numbers_to_words(self, tokens: List[str]) -> List[str]:
-        """Replace all occurrences of arabic numerals by their
-        full, word-based counterparts.
+    def _numbers_to_words(self, text: str) -> str:
+        """Replace all occurrences of numbers by their full, word-based counterparts.
+        This includes whole numbers and real point numbers, with an optional '+' or '-' prefix.
 
-        Comma's and 'and' words and hyphons to connect the numbers-words
-        together are purposely removed, so that only the number
-        words themselves remain. This is to avoid adding additional
-        special characters and stop-words to the token list.
+        Comma's, 'and' words and hyphons ('-' symbols) to connect the
+        number words together are purposely excluded from the output,
+        so that only the number words themselves remain. This prevents
+        adding additional special characters and stop-words to the output.
 
         e.g.
-            >>> self._numbers_to_words(["123", 4])
-            ["one", "thousand", "one", "hundred", "twenty", "three", "four"]
 
-        :param tokens: The text in which to replace all numerals by words, as a list of tokens
-        :return: The converted text, as a pruned version of the input *tokens*
+            >>> vsm = VSMTokenizer()
+        
+            >>> vsm._numbers_to_words("1123")
+            "one thousand one hundred twenty three"
+
+            >>> vsm._numbers_to_words("-1 2")
+            "minus one two"
+
+            >>> vsm._numbers_to_words("PI UP TO FOUR DIGITS IS 3.1415")
+            "PI UP TO FOUR DIGITS IS three point one four one five"
+
+            >>> vsm._numbers_to_words("-1.4&+2")
+            "minus one point four&plus two"
+
+        :param text: The text in which to replace all numbers by words
+        :return: The modified text
         """
-        return [
-            transformed_token
-            for token in tokens
-                for transformed_token in\
-                (
-                    self._inflect_engine.number_to_words(token, andword='').\
-                        replace(',', '').\
-                        replace('-', ' ').\
-                        split()
-                    if self._is_number(token) else [token]
-                )
-        ]
+        def transform_match(number: re.Match) -> str:
+            """Map each number match to its word form."""
+            number_text: str = number.group(0)
+            return self._inflect_engine.number_to_words(number_text, andword='').\
+                replace(',', '').\
+                replace('-', ' ')
+
+        expanded_text: str = self._NUMBER_PATTERN.sub(transform_match, text)
+        return expanded_text
+
 
     def _stem(self, tokens: List[str]) -> List[str]:
         """Apply stemming to the *tokens*.
@@ -131,13 +141,13 @@ class VSMTokenizer:
         """
         return [self._stemmer.stem(word) for word in tokens]
 
-    def _remove_stopwords(self, tokens: List[str], is_lower_case: bool = False):
-        """Strip the text of any instances of a stopword.
+    def _remove_stopwords(self, tokens: List[str], is_lower_case: bool = False) -> List[str]:
+        """Strip the *tokens* list of any instances of a stopword.
         
         This method was obtained from the following article and subsequently adapted:
             https://www.kdnuggets.com/2018/08/practitioners-guide-processing-understanding-text-2.html
 
-        :param tokens: The text, as a list of *tokens*, to strip of stopwords
+        :param tokens: The text, as a list of *tokens*, to filter of stopwords
         :param is_lower_case: Whether the *tokens* are all already entirely in lower case
         :return: The *tokens* stripped (pruned) of (all) stopwords
         """
@@ -149,32 +159,13 @@ class VSMTokenizer:
 
         return filtered_tokens
 
-    def _load_stopwords(self) -> None:
+    def _load_stopwords(self) -> Set[str]:
         """Load the nltk english stopwords list.
 
         :raise LookupError: If the stopword list could not be located nor downloaded
         """
         try:
-            self._stopword_list = set(nltk.corpus.stopwords.words("english"))
+            return set(nltk.corpus.stopwords.words("english"))
         except LookupError:
             nltk.download("stopwords")
-            self._stopword_list = set(nltk.corpus.stopwords.words("english"))
-
-    def _is_number(self, text: str):
-        """Check whether the given *text* is a pure number string.
-        This can be either a whole number, or a float.
-
-        This method was based on the following SO post:
-            https://stackoverflow.com/questions/354038/how-do-i-check-if-a-string-represents-a-number-float-or-int
-
-        :param text: The string for which to check if it is number as a string
-        :return: True if *text* contains only a number, else False
-        """
-        try:
-            if text.isnumeric():
-                return True
-            float(text)
-            return True
-        except ValueError:
-            return False
-
+            return set(nltk.corpus.stopwords.words("english"))
