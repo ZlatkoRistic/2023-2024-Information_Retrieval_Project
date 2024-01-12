5fa02bd363a26711f1328a2eb90966ddf6689f00
Bas <bas.tobback@student.uantwerpen.be>
2024-01-12 20:03:03 +0100

    documentation for API endpoints in views for flask webpage.

--- views.py
+++ views.py
@@ -23,11 +23,30 @@ with open(VSM_INDEX_DUMP_PATH, "r", encoding="utf8") as index_file:
 
 @views.route("/")
 def home():
+    """The route to the home page.
+    This is the main page we see when starting the application and offers us access to all functionalities we would want to have available.
+
+    :return: render_template of the index.html home page
+    """
     return render_template("index.html")
 
 
 @views.route("/submit", methods=["GET"])
 def submit():
+    """Route to submitting a claim to be checked.
+    This route shall check the claim against evidence and display the result.
+
+    Using the claim, this will retrieve the most relevant pages using a VSM.
+    Then use the one most relevant page to check if the claim is supported by reading the content and passing it to GPT.
+    Using the GPT model the claim is validated using the evidence(list of strings that are the names of evidence file)
+
+    arguments:
+    - claim: a statement which has to be verified by the evidence available using a GPT model
+
+    The rendered template contains the claim, the result and a list of buttons that show evidence in new pages.
+
+    :return: rendered template of evidence.html containing the claim, result and list of evidence buttons
+    """
     args = request.args
     claim = args.get('claim')
     k: int = 3
@@ -61,6 +80,17 @@ def submit():
 
 @views.route("/show_document", methods=["GET"])
 def show_document():
+    """Route to the evidence documents.
+    This route takes an argument which should be the name of a document in the provided evidence and return it rendered.
+
+    This document should be a html file with name 'x.html' where x is the argument passed in this function.
+    The document will be rendered from the folder 'raw-articles' and the html extension should NEVER be in the argument.
+
+    arguments:
+    - pagenr: the name of the file, which must be a html file. Ex.: 'example.html', pagenr should be 'example'
+
+    :return: rendered html file from render_template
+    """
     args = request.args
     page = args.get('pagenr')
 
@@ -71,6 +101,19 @@ def show_document():
 
 @views.route("/evaluate", methods=["GET"])
 def evaluate():
+    """Route for the evaluation extension of index.html.
+    This route inserts the evaluation.html blocks into the blocks of index.html.
+
+    Using the input and output arguments this route calls the verification from utils and checks the precision.
+    The precision, also from utils, uses the correct and total outputs from verification to return the precision.
+    This returned precision is fed into evaluation.html, which is then rendered and returned
+
+    arguments:
+    - input: relative path from the working directory to the input file to be used in the verification
+    - output: relative path from the working directory to the output file to be used in the verification
+
+    :return: rendered template with evaluation.html with a calculated precis value given
+    """
     args = request.args
     eval_input = args.get("input")
     eval_output = args.get("output")
